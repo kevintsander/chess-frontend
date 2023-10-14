dba3d3ffb2ee0e15668f61f98b3bffbf0d48fe07
Kevin Sander <kevintsander@gmail.com>
2023-10-14 15:30:28 -0400

    added selected unit logic

--- src/app/unit/unit.component.ts
+++ src/app/unit/unit.component.ts
@@ -1,6 +1,7 @@
 import { Component, Input } from '@angular/core';
 import { Unit } from './unit.model';
 import { GameStateService } from '../game-state/game-state.service';
+import { TurnStateService } from '../turn-state.service';
 
 @Component({
   selector: 'app-unit',
@@ -10,4 +11,16 @@ import { GameStateService } from '../game-state/game-state.service';
 export class UnitComponent {
   @Input({ required: true }) unit!: Unit;
   @Input() gameStateService!: GameStateService;
+
+  constructor(protected turnStateService: TurnStateService) { }
+
+  onClick(): void {
+    if (this.gameStateService.isSelectable(this.unit.location)) {
+      if (this.turnStateService.selectedUnit === this.unit) {
+        this.turnStateService.deselectUnit();
+      } else {
+        this.turnStateService.selectUnit(this.unit);
+      }
+    }
+  }
 }
