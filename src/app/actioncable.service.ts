b803fd8d141cd298d03019f6d5b0d6bf1f5da101
Kevin Sander <kevintsander@gmail.com>
2023-10-25 23:07:55 -0400

    improved action cable subscription logic

--- src/app/actioncable.service.ts
+++ src/app/actioncable.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import * as ActionCable from '@rails/actioncable';
-import { Subject, Subscription } from 'rxjs';
+import { Observable, Subject, Subscription } from 'rxjs';
 import { Game } from './game/game.model';
 @Injectable({
   providedIn: 'root'
@@ -9,25 +9,29 @@ export class ActionCableService {
   private consumer: any;
   receivedSubject: Subject<Game> = new Subject<Game>();
 
-  constructor() { }
-
-  subscribe(game_id: string) {
-    this.consumer = ActionCable.createConsumer('http://localhost:3000/cable');
-    this.consumer.subscriptions.create({ channel: 'GameChannel', game_id: game_id }, {
-      connected: () => {
-        console.log('connected!')
-      },
-      disconnected: () => {
-        console.log('disconnected')
-      },
-      received: (data: any) => {
-        this.receivedSubject.next(data.body);
-      }
-
-    })
+  constructor() {
+    this.consumer = ActionCable.createConsumer('http://localhost:3000/cable'); // TODO - move to config
   }
 
-  getReceivedSubject(): Subject<Game> {
-    return this.receivedSubject;
+  getGame$(game_id: string): Observable<Game> {
+    const game$ = new Observable<Game>(subscriber => {
+      const channel = this.consumer.subscriptions.create({ channel: 'GameChannel', game_id: game_id }, {
+        connected: () => {
+          console.log(`connected to game channel (game_id: '${game_id})`);
+        },
+        disconnected: () => {
+          console.log(`disconnected from game channel (game_id: '${game_id})`);
+          // TODO: throw error here? this means server disconnected the channel.
+        },
+        received: (data: any) => {
+          subscriber.next(data);
+        }
+      });
+
+      return () => {
+        channel.unsubscribe();
+      }
+    });
+    return game$;
   }
 }
--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -1,32 +1,33 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { GameApiService } from './game-api.service';
 import { Game } from './game.model';
 import { GameStateService } from '../game-state/game-state.service';
 import { ActionCableService } from '../actioncable.service';
+import { Observable, Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-game',
   templateUrl: './game.component.html',
   styleUrls: ['./game.component.scss']
 })
-export class GameComponent {
+export class GameComponent implements OnDestroy {
   chessGame: Game | undefined;
   gameStateService!: GameStateService;
+  currentGameSub!: Subscription;
 
   constructor(public gameService: GameApiService, public actionCableService: ActionCableService) { }
 
   loadGame(id: string) {
-    this.gameService.getGame(id)
+    this.currentGameSub?.unsubscribe(); // unsubscribe to any already loaded game
+    this.currentGameSub = this.actionCableService.getGame$(id)
       .subscribe((response) => {
         this.chessGame = response;
         this.gameStateService = new GameStateService(this.chessGame.game_state);
       });
+  }
 
-    this.actionCableService.subscribe(id);
-    this.actionCableService.getReceivedSubject().subscribe(result => {
-      this.chessGame = result;
-      this.gameStateService.gameState = this.chessGame.game_state;
-    })
+  ngOnDestroy(): void {
+    this.currentGameSub?.unsubscribe();
   }
 
 }
