dd6a89ed01d4688b0712538e2c8172e3a04c719b
Kevin Sander <kevintsander@gmail.com>
2024-10-06 18:28:32 -0400

    updated api url

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  chessApiUrl: "https://cntapp-sander-chess-dev-01.lemonplant-3486f733.northcentralus.azurecontainerapps.io/",
-  chessActionCableUrl: "https://cntapp-sander-chess-dev-01.lemonplant-3486f733.northcentralus.azurecontainerapps.io/cable"
+  chessApiUrl: "https://cntapp-sander-chess-dev-02.yellowpond-f0808ae4.northcentralus.azurecontainerapps.io/",
+  chessActionCableUrl: "https://cntapp-sander-chess-dev-02.yellowpond-f0808ae4.northcentralus.azurecontainerapps.io/cable"
 
 };
